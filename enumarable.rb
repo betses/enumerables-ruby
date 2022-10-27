92da13ff9441bda121a713b76130c6115c447421
GordonOtieno <otienogordon95@gmail.com>
2022-10-27 17:19:41 +0300

    Update enumerable file

--- enumarable.rb
+++ enumarable.rb
@@ -9,4 +9,19 @@ module Enumerables
   def any?; end
 
   def filter?; end
+
+  def any?
+    each do |element|
+      return true if yield(element)
+    end
+    false
+  end
+  def filter
+    array = []
+    each do |element|
+      array << element if yield(element)
+    end
+    array
+  end
+  end
 end
